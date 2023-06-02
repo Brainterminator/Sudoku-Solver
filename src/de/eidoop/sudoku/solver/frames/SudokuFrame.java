4781e4e53f0a8688e98e22a08bf997d759e8aa8f
Leo Sulzbacher <labrigestoast.ls@gmail.com>
2023-06-02 23:22:33 +0200

    Restructuring the Code to match the structure of the app

--- src/de/eidoop/sudoku/solver/frames/SudokuFrame.java
+++ src/de/eidoop/sudoku/solver/frames/SudokuFrame.java
@@ -3,10 +3,10 @@ package de.eidoop.sudoku.solver.frames;
 import de.eidoop.sudoku.api.entities.Sudoku;
 import de.eidoop.sudoku.api.enums.SudokuState;
 import de.eidoop.sudoku.api.exceptions.*;
+import de.eidoop.sudoku.api.loader.ExampleLoader;
+import de.eidoop.sudoku.api.loader.SudokuLoader;
 import de.eidoop.sudoku.api.solver.BruteForceSolver;
-import de.eidoop.sudoku.api.solver.RandomSolver;
 import de.eidoop.sudoku.api.solver.SaveSolver;
-import de.eidoop.sudoku.api.solver.Solver;
 import de.eidoop.sudoku.api.ui.ISudokuRenderer;
 import de.eidoop.sudoku.solver.components.SudokuButton;
 
@@ -20,17 +20,19 @@ public class SudokuFrame extends Frame implements ISudokuRenderer {
 
     private String currentError = "";
     private int currentX, currentY, currentValue;
-    private Solver solver;
     private final TextField text;
     private final Sudoku sudoku;
 
     public SudokuFrame(Sudoku sudoku) {
         super("Sudoku App");
+        int buttonSize = 25;
+        int elementWidth = 9*buttonSize;
         int height = 545;
-        int width = 245;
+        int width = elementWidth + 20;
+        int offset = 31;
+        int gridSize = 2;
         this.setSize(width, height);
         this.sudoku = sudoku;
-        this.solver = new BruteForceSolver(sudoku);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -42,115 +44,11 @@ public class SudokuFrame extends Frame implements ISudokuRenderer {
         this.setResizable(false);
 
 
-        // Schaltflächen generieren und platzieren
-
-        // Sudoku Felder
-        int buttonGroesse = 25;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                // Neue Schaltfläche instanziieren
-                SudokuButton b = new SudokuButton(i, j, sudoku);
-                b.setLabel("");
-                // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
-                b.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        currentX = ((SudokuButton) e.getSource()).x;
-                        currentY = ((SudokuButton) e.getSource()).y;
-                        text.requestFocus();
-                    }
-                });
-                // Position auf dem Elternelement (x, y, breite, hoehe)
-                b.setBounds(i * buttonGroesse + 10, j * buttonGroesse + 310, buttonGroesse, buttonGroesse);
-                this.add(b);
-            }
-        }
-
-        //Lösen Button
-
-        Button loesenButton = new Button();
-        loesenButton.setLabel("Lösen");
-        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
-        loesenButton.addActionListener(e -> {
-            solver.solve();
-            currentError = "";
-            update();
-        });
-        loesenButton.setBounds(10, 50 + buttonGroesse + 10, buttonGroesse * 4, buttonGroesse);
-        this.add(loesenButton);
-
-        //Neu Button
-
-        Button neuButton = new Button();
-        neuButton.setLabel("Neu");
-        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
-        neuButton.addActionListener(e -> {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sudoku.setEmpty(i, j);
-                }
-            }
-            sudoku.setState(SudokuState.LOADED);
-            currentError = "";
-            update();
-        });
-        neuButton.setBounds(10 + buttonGroesse * 4 + 10, 50 + buttonGroesse + 10, buttonGroesse * 4, buttonGroesse);
-        this.add(neuButton);
-
-        //Probier Button
-
-        Button probierButton = new Button();
-        probierButton.setLabel("Probieren");
-        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
-        probierButton.addActionListener(e -> setLoeser(new BruteForceSolver(sudoku)));
-        probierButton.setBounds(10, 110 + buttonGroesse + 10, buttonGroesse * 4, buttonGroesse);
-        this.add(probierButton);
-
-        //Zufall Button
-
-        Button zufallButton = new Button();
-        zufallButton.setLabel("Zufall");
-        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
-        zufallButton.addActionListener(e -> setLoeser(new RandomSolver(sudoku)));
-        zufallButton.setBounds(10, 140 + buttonGroesse + 10, buttonGroesse * 4, buttonGroesse);
-        this.add(zufallButton);
-
-        //Strategie Button
-
-        Button strategieButton = new Button();
-        strategieButton.setLabel("Strategie");
-        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
-        strategieButton.addActionListener(e -> setLoeser(new SaveSolver(sudoku)));
-        strategieButton.setBounds(10, 170 + buttonGroesse + 10, buttonGroesse * 4, buttonGroesse);
-        this.add(strategieButton);
+        // Generate elements and assign positions
 
 
-        // Fehleranzeige
+        // Text Field
 
-        Label fehlerAnzeige = new Label() {
-            @Override
-            public void paint(Graphics g) {
-                super.paint(g);
-                setText(currentError);
-            }
-        };
-        fehlerAnzeige.setBounds(10, 80 + buttonGroesse + 10, buttonGroesse * 8, buttonGroesse);
-        this.add(fehlerAnzeige);
-
-        // Zustand
-
-        Label zustand = new Label() {
-            @Override
-            public void paint(Graphics g) {
-                super.paint(g);
-                setText(sudoku.getState().getStateName());
-            }
-        };
-        zustand.setBounds(10, 230 + buttonGroesse + 10, buttonGroesse * 4, buttonGroesse);
-        this.add(zustand);
-
-
-        // Text Feld
         text = new TextField();
         text.addActionListener(e -> {
             currentValue = Integer.parseInt(((TextField) e.getSource()).getText());
@@ -167,40 +65,136 @@ public class SudokuFrame extends Frame implements ISudokuRenderer {
                     }
                 }
             } catch (QuadrantOccupiedException ex) {
-                currentError = "Quadrant belegt!";
+                currentError = "Quadrant occupied!";
             } catch (ColumnOccupiedException ex) {
-                currentError = "Spalte belegt!";
+                currentError = "Column occupied!";
             } catch (RowOccupiedException ex) {
-                currentError = "Zeile belegt!";
+                currentError = "Row occupied!";
             } catch (ValueOutOfRangeException ex) {
-                currentError = "Ungültiger Wertebereich!";
+                currentError = "Value out of range!";
             } catch (FieldFixedException ex) {
-                currentError = "Feld belegt!";
+                currentError = "Field already occupied!";
             } catch (CoordinatesOutOfRangeException ex) {
-                currentError = "Coordinates out of Range!";
+                currentError = "Coordinates out of range!";
             }
             text.setText("");
-            update();
-
+            print(sudoku);
         });
-        text.setBounds(10, 50, buttonGroesse * 9, buttonGroesse);
+        text.setBounds(10, offset, buttonSize * 9, buttonSize);
         this.add(text);
 
 
+        // Error display
+
+        Label errorDisplay = new Label() {
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                setText(currentError);
+            }
+        };
+        errorDisplay.setBackground(Color.LIGHT_GRAY);
+        errorDisplay.setBounds(10, offset + (buttonSize + gridSize), buttonSize * 9, buttonSize);
+        this.add(errorDisplay);
+
+
+        // New Button
+
+        Button emptyButton = new Button();
+        emptyButton.setLabel("New Empty");
+        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
+        emptyButton.addActionListener(e -> {
+            sudoku.reset();
+            currentError = "";
+            print(sudoku);
+        });
+        emptyButton.setBounds(10, offset + 2*(buttonSize + gridSize), buttonSize * 9, buttonSize);
+        this.add(emptyButton);
+
+
+        // Example Button
+
+        Button exampleButton = new Button();
+        exampleButton.setLabel("New Example");
+        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
+        exampleButton.addActionListener(e -> {
+            new ExampleLoader().loadSudoku(sudoku);
+            currentError = "";
+            print(sudoku);
+        });
+        exampleButton.setBounds(10, offset + 3*(buttonSize + gridSize), buttonSize * 9, buttonSize);
+        this.add(exampleButton);
+
+
+
+        // Zustand
+
+        Label zustand = new Label() {
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                setText(sudoku.getState().getStateName());
+            }
+        };
+        zustand.setBounds(10, 180 + offset + buttonSize + 10, buttonSize * 9, buttonSize);
+        this.add(zustand);
+
+
+        //Probier Button
+
+        Button probierButton = new Button();
+        probierButton.setLabel("Probieren");
+        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
+        probierButton.addActionListener(e -> {
+            new BruteForceSolver(sudoku).solve();
+            print(sudoku);
+        });
+        probierButton.setBounds(10, 60 + offset + buttonSize + 10, buttonSize * 4, buttonSize);
+        this.add(probierButton);
+
+        //Strategie Button
+
+        Button strategieButton = new Button();
+        strategieButton.setLabel("Strategie");
+        // ActionListener als anonyme Klasse mit Referenz auf lokale Variable
+        strategieButton.addActionListener(e -> {
+            new SaveSolver(sudoku).solve();
+            print(sudoku);
+        });
+        strategieButton.setBounds(10, 120 + offset + buttonSize + 10, buttonSize * 4, buttonSize);
+        this.add(strategieButton);
+
+
+        // Sudoku Fields
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                // Generate single buttons
+                SudokuButton b = new SudokuButton(i, j, sudoku);
+                b.setLabel("");
+                b.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentX = ((SudokuButton) e.getSource()).x;
+                        currentY = ((SudokuButton) e.getSource()).y;
+                        text.requestFocus();
+                    }
+                });
+                b.setBounds(i * buttonSize + 10, j * buttonSize + 260 + offset, buttonSize, buttonSize);
+                this.add(b);
+            }
+        }
+
+
+
+
         this.setVisible(true);
     }
 
-    void update() {
-        this.setVisible(false);
-        this.setVisible(true);
-    }
-
-    void setLoeser(Solver solver){
-        this.solver= solver;
-    }
     @Override
     public void print(Sudoku sudoku) {
-
+        this.setVisible(false);
+        this.setVisible(true);
     }
 }
 
