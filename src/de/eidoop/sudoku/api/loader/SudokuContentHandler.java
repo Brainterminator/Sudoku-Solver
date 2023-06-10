a5c5c47dbde2955b02a694b7a5b964acd429e46d
Fabian Hötschl <fh019637@fh-muenster.de>
2023-06-10 16:46:45 +0200

    Added XMLLoader

---
+++ src/de/eidoop/sudoku/api/loader/SudokuContentHandler.java
@@ -0,0 +1,50 @@
+package de.eidoop.sudoku.api.loader;
+
+import de.eidoop.sudoku.api.entities.Sudoku;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SudokuContentHandler extends DefaultHandler {
+    private StringBuilder cellValue;
+    private Sudoku sudoku;
+    private int row = 1;
+    private int col = 1;
+
+    public SudokuContentHandler(Sudoku sudoku) {
+        this.sudoku = sudoku;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        if (qName.equals("cell")) {
+            cellValue = new StringBuilder();
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) {
+        if (cellValue != null) {
+            cellValue.append(ch, start, length);
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) {
+        try {
+            if (!qName.equals("cell"))
+                return;
+
+            if (col > 9) {
+                col = 1;
+                row++;
+            }
+
+            int value = cellValue.length() > 0 ? Integer.parseInt(cellValue.toString().trim()) : 0;
+            if (value != 0)
+                sudoku.setFixedValue(row, col, value);
+            
+            col++;
+        } catch (Exception e) {
+        }
+    }
+}
--- src/de/eidoop/sudoku/api/loader/XMLLoader.java
+++ src/de/eidoop/sudoku/api/loader/XMLLoader.java
@@ -2,6 +2,17 @@ package de.eidoop.sudoku.api.loader;
 
 import de.eidoop.sudoku.api.entities.Sudoku;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 public class XMLLoader extends SudokuLoader{
     private String path;
     public XMLLoader(String path) {
@@ -11,6 +22,32 @@ public class XMLLoader extends SudokuLoader{
     @Override
     public void loadSudoku(Sudoku sudoku) {
         sudoku.reset();
-        //Code here that places the Values inside the sudoku
+
+        // Validiere das XML-Dokument gegen das XML-Schema
+        if (!validateXml())
+            return;
+
+        // Lade das Sudoku aus der XML-Datei
+        try {
+            InputStream xmlInputStream = new FileInputStream(path);
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(xmlInputStream, new SudokuContentHandler(sudoku));
+        } catch(Exception e){ }
+    }
+
+    private boolean validateXml() {
+        try {
+            // Erstelle ein Schema-Objekt basierend auf dem XML-Schema
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File("sudoku.xsd"));
+
+            // Validiere das XML-Dokument gegen das Schema
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(path)));
+            return true; // Das XML-Dokument ist valide
+        } catch (Exception e) {
+            return false; // Das XML-Dokument ist nicht valide
+        }
     }
 }
